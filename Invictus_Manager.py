0a84672d83a24f359c0e5a9fe50ad1c175227053
Dostroyer07 <joseph-rogers@comcast.net>
2016-03-31 23:14:50 -0500

    Added files via upload

---
+++ Invictus_Manager.py
@@ -0,0 +1,51 @@
+print ('=========================================')
+print ('Hello, welcome to "Invictus" librarian A.I.')
+print ('Last Used: 12/31/16') 
+print ('Last Used by:')
+print ('Last Command:')
+print ('=========================================')
+print ('Please insert Login:')
+if input() == 'Dostroyer07' or 'Samthecow123' or 'Diego' or 'Major Kolton' or 'Blightcaller' or 'Phoenix' or 'Greyraska' or 'Blacklight1234':
+     print ('Please enter Password')              
+if input() == 'TrinaryUnion15':
+     print ('Login Successful, Welcome')
+     print ('Data Contained: 1) Base Coordinates, 2) Blueprints, 3) Scripts,')
+     print ('4) Foreign Relations, 5) Spies, 6) logs. To access, please type in the res')
+     print ('pective number in order (I.e. 1,2,3,4, not 1,4.')
+     if input() == '1':
+         print ('No coords input')
+     if input() == '2':
+          print ('Angler[Url=], Sparrow[url=], Dragonstar[url=], Hatsuharu[url=]')
+          print ('Terminus[url=], Reaper [url=], Horizon[url=], Titan[url=], Nagflar[url=],')
+          print ('Argonev[url=], Broadsword[url=],')
+     if input() == '3':
+         print ('Hacker[url=], Homing missiles[url=], Inventory Sorter[url=],')
+         print ('Weapon Sequencer[url=], ')
+     if input() == '4':
+        print ('ISMC: Hostile, UEMC: Allied, ISB: Allied, NCFSC: Hostile to,')
+        print ('UASC: Hostile, DS-A: Hostile, SSA: Hostile, UPDR: Dead, CAS: Dead,')
+        print ('TSU: Dead, USF: Dead.') 
+     if input() == '5':
+        print ('Invictus, Additional Clearence Required, please input key')
+        if input() == '19624IJ':
+            print ('AlexlikesAyylmao: ISMC, Velociraptor: Undeployed, Scourgewar: ISMC, Radicator: ISMC')
+     if input() == '7':
+        print ('Invictus, Additional Clearence Required, please input key')
+        if input() == '1936J':
+            print ('We will declare war on ISB ASAP, ISMC war is going well, NCFSC will be finished,')
+            print ('Soon enough, the TSU will be reformed')
+     if input() == '6':
+       print ('Invictus, Additional Clearence Required, please input key')
+       if input() == '1936J67':
+            print ('Operation "4th of July",')
+            print ('Led by: Blightcaller')
+            print ('Participants: EgorKaskader, Paradox, Blightcaller, Velociraptor, AlexlikesAyylmao')
+            print ('Description: Raid on ISMC Bunker, highly effective. No losses, all win.')
+else:
+    print ('Invictus, Access Denied, Program will terminate')
+    quit()
+    exit
+
+
+
+    
